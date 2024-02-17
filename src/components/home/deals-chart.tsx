d96a4dc00a1cbd7a77ccc61b0ac68045d1ea8780
Darryl <darrylestrada97@gmail.com>
2024-02-17 23:03:58 +0100

    added dashboard chart

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,7 +1,80 @@
+import { DollarOutlined } from "@ant-design/icons";
+import { Card } from "antd";
 import React from "react";
+import { Text } from "../text";
+import { Area, AreaConfig } from "@ant-design/plots";
+import { useList } from "@refinedev/core";
+import { DASHBOARD_DEALS_CHART_QUERY } from "@/graphql/queries";
+import { mapDealsData } from "@/utilities/helpers";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { DashboardDealsChartQuery } from "@/graphql/types";
 
 const DealsChart = () => {
-  return <div>deals-chart</div>;
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+    resource: "dealStages",
+    filters: [
+      {
+        field: "title",
+        operator: "in",
+        value: ["WON", "LOST"],
+      },
+    ],
+    meta: {
+      gqlQuery: DASHBOARD_DEALS_CHART_QUERY,
+    },
+  });
+  const dealData = React.useMemo(() => {
+    return mapDealsData(data?.data || []);
+  }, [data?.data]);
+  const config: AreaConfig = {
+    data: dealData,
+    xField: "timeText",
+    yField: "value",
+    isStack: false,
+    seriesField: "state",
+    animation: true,
+    startOnZero: false,
+    smooth: true,
+    legend: { position: "top", offsetY: 10 },
+    yAxis: {
+      tickCount: 4,
+      label: { formatter: (v) => `$${Number(v) / 1000}K` },
+    },
+    tooltip: {
+      formatter: (data) => {
+        return { name: data.state, value: `$${Number(data.value) / 1000}K` };
+      },
+    },
+  };
+  return (
+    <Card
+      style={{ height: "100%" }}
+      styles={{
+        header: {
+          backgroundColor: "#87d",
+          color: "#ffff",
+          padding: "8px 16px",
+        },
+        body: { padding: "24px 24px 0 24px" },
+      }}
+      title={
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+          }}
+        >
+          <DollarOutlined />
+          <Text size="sm" style={{ marginLeft: "0.5rem", color: "#ffff" }}>
+            Deals
+          </Text>
+        </div>
+      }
+    >
+      <Area {...config} height={325} />
+    </Card>
+  );
 };
 
 export default DealsChart;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,16 @@
 import UpcomingEvents from "./home/upcoming-events";
 import DealsChart from "./home/deals-chart";
-export { UpcomingEvents, DealsChart };
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+export {
+  UpcomingEvents,
+  DealsChart,
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  ProjectCardSkeleton,
+  LatestActivitiesSkeleton,
+};
