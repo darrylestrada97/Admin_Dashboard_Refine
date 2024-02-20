c7de3a23f3d9551cecc270fddd1c61f5c5b1eeee
Darryl <darrylestrada97@gmail.com>
2024-02-20 21:43:26 +0100

    added the total count and fixed the card source input from contracts to contacts

--- src/components/home/total-count-card.tsx
+++ src/components/home/total-count-card.tsx
@@ -1,13 +1,18 @@
+import { totalCountVariants } from "@/constants";
 import { icons } from "antd/es/image/PreviewGroup";
 import { Card } from "antd/lib";
 import React from "react";
+import { Text } from "../text";
+import { Skeleton } from "antd";
 
 type Props = {
-  resources: "companies" | "contacts" | "deals";
+  resource: "companies" | "contacts" | "deals";
   isLoading: boolean;
   totalCount: number;
 };
 const DashboarTotalCountCard = ({ resource, isLoading, totalCount }: Props) => {
+  const { primaryColor, secondaryColor, icon, title } =
+    totalCountVariants[resource];
   return (
     <Card
       style={{ height: "96px", padding: 0 }}
@@ -21,7 +26,13 @@ const DashboarTotalCountCard = ({ resource, isLoading, totalCount }: Props) => {
           whiteSpace: "nowrap",
         }}
       >
-        {icons[resource]}
+        {icon}
+        <Text size="md" className="secondary" style={{ marginLeft: "8px" }}>
+          {title}
+        </Text>
+      </div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Text>{isLoading ? <Skeleton.Button /> : totalCount}</Text>
       </div>
     </Card>
   );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -29,7 +29,7 @@ export const Home = () => {
         </Col>
         <Col xs={24} sm={24} xl={8}>
           <DashboarTotalCountCard
-            resource="contracts"
+            resource="contacts"
             isLoading={isLoading}
             totalCount={data?.data.contacts.totalCount}
           />
