2dddd81ce6068c0e19668985fb1f56fe8c8180cf
Darryl <darrylestrada97@gmail.com>
2024-02-23 21:49:57 +0100

    added usetable and gqlquery

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -1,10 +1,39 @@
+import { COMPANIES_LIST_QUERY } from "@/graphql/queries";
 import { CreateButton, List } from "@refinedev/antd";
+import { useTable } from "@refinedev/antd";
 import { useGo } from "@refinedev/core";
+import { Table } from "antd";
 import React from "react";
 
 export const CompanyList = () => {
   const go = useGo();
+  const { tableProps, filters } = useTable({
+    resource: "companies",
+    meta: {
+      gqlQuery: COMPANIES_LIST_QUERY,
+    },
+  });
   return (
-    <List breadcrumb={false} headerButtons={() => <CreateButton />}></List>
+    <List
+      breadcrumb={false}
+      headerButtons={() => (
+        <CreateButton
+          onClick={() => {
+            go({
+              to: {
+                resource: "companies",
+                action: "create",
+              },
+              options: {
+                keepQuery: true,
+              },
+              type: "replace",
+            });
+          }}
+        />
+      )}
+    >
+      <Table></Table>
+    </List>
   );
 };
