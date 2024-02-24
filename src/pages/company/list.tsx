316208f9edf797014351554785fe310f3397c26a
Darryl <darrylestrada97@gmail.com>
2024-02-24 23:58:13 +0100

    deleted the test

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -39,7 +39,6 @@ export const CompanyList = () => {
         />
       )}
     >
-      <div>test</div>
       <Table {...tableProps} pagination={{ ...tableProps.pagination }}>
         <Table.Column
           dataIndex="name"
