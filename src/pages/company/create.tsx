4c64e3b09d2343b4c2741e2a1e31a0b2e2d0034e
Darryl <darrylestrada97@gmail.com>
2024-02-26 21:01:13 +0100

    added goto Method to create

--- src/pages/company/create.tsx
+++ src/pages/company/create.tsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { CompanyList } from "./list";
+import { Modal } from "antd";
+import { useModalForm } from "@refinedev/antd";
+import { useGo } from "@refinedev/core";
 
 export const Create = () => {
-  return <div>Create Casdasdreate Create</div>;
+  const go = useGo();
+
+  const goToListPage = () => {
+    go({
+      to: {
+        resource: "companies",
+        action: "list",
+      },
+      options: { keepQuery: true },
+      type: "replace",
+    });
+  };
+
+  const { formProps, modalProps } = useModalForm({
+    action: "create",
+    defaultVisible: true,
+    resource: "companies",
+    redirect: false,
+    mutationMode: "pessimistic",
+    onMutationSuccess: () => {},
+  });
+  return (
+    <CompanyList>
+      <Modal></Modal>
+    </CompanyList>
+  );
 };
